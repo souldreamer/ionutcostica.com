7fe7e6e525c6c5f39ddf2cd4c477d193999daad4
Ionut Costica <ionut.costica@gmail.com>
2015-06-03 18:10:49 +0300

    making www.site.com work, not just site.com

--- server.js
+++ server.js
@@ -4,8 +4,10 @@
 var express = require('express');
 var evh = require('express-vhost');
 
+var appFactories = {};
 var appFactory = function(website) {
-    return require('./' + website + '/server');
+	if (appFactories[website] === undefined) appFactories[website] = require('./' + website + '/server');
+	return appFactories[website];
 };
 
 var server = express();
@@ -15,5 +17,7 @@ var serve = server.listen(80, function () {
 });
 
 evh.register('localhost', appFactory('ionutcostica.com'));
+evh.register('www.ionutcostica.com', appFactory('ionutcostica.com'));
 evh.register('ionutcostica.com', appFactory('ionutcostica.com'));
+evh.register('www.iuliamihet.com', appFactory('iuliamihet.com'));
 evh.register('iuliamihet.com', appFactory('iuliamihet.com'));
